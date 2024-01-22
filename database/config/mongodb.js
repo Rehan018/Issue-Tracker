b3f761813494fb109ab1f2b0c90fdc7e349c9d97
Md Rehan Alam <76740248+Rehan018@users.noreply.github.com>
2024-01-22 12:20:02 +0530

    Update mongodb.js

--- database/config/mongodb.js
+++ database/config/mongodb.js
@@ -4,8 +4,8 @@ const url = 'mongodb://localhost:27017/';
 
 async function getEmpData() {
     const client = await MongoClient.connect(url);
-    const connection = client.db('issueTracker'); // Establish connection
-    return connection.collection('IssueTracker2023'); // creating a collection and naming it also
+    const connection = client.db('issueTracker'); 
+    return connection.collection('IssueTracker'); 
 }
 
 export default getEmpData;
